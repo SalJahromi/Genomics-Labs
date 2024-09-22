e733e4911379eb0e98af96036224272b7b420d1b
SalJahromi <saljahromi@gmail.com>
2024-09-21 21:10:52 -0700

    scatterplot of hp vs mpg created

--- Lab2_GitHubRFile.R
+++ Lab2_GitHubRFile.R
@@ -4,5 +4,8 @@
 data("mtcars")
 vroom <- mtcars
 
-#check class of vroom
+# check class of vroom
 class(vroom)
+
+# scatterplot of hp on y and mpg on x
+plot(vroom$mpg,vroom$hp, main = "Horsepower vs Mileage")
